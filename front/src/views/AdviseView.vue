b9e8ab247ec8dc3627be0eb29bd995eb20f48c0e
jacuminthomas666 <thomas.jacumin@uha.fr>
2022-08-04 20:40:03 +0200

    Put text in advise page

--- front/src/views/AdviseView.vue
+++ front/src/views/AdviseView.vue
@@ -3,5 +3,24 @@
 </script>
 
 <template>
-  advise
+	<p>
+  	Cette page a pour objectif de donner quelques conseils pour faire un exposé qui soit adapté à l’auditoire et qui évite les écueils les plus fréquents.
+  </p>
+  <p>
+  	<ul>
+  		<li>pas plus d'une slide par minutes ;</li>
+  		<li>ne pas charger trop les slides ;</li>
+  		<li>ne pas chercher à faire une revue exhaustive de la littérature ;</li>
+  		<li>ne pas chercher à présenter tout vos résultats. Un seul résultat parlant et bien détaillé suffit ;</li>
+  		<li>ne pas hésiter à détailler un exemple ;</li>
+  		<li>essayer de donner de l'intuition par tout les moyens (exemple, schémas, dessins, calcul simple, etc...) ;</li>
+  		<li>quand vous utilisez du vocabulaire techniques, des abréviations ou des notations qui ne sont pas universelles, prenez le temps de l'expliquer à l'auditoire ;</li>
+  		<li>quand vous utilisez une formule, prenez quelques secondes pour l'expliquer ;</li>
+  		<li>quand vous présentez une illustration, expliquer bien ce que vous montrer (paramètres, variables, etc...) ;</li>
+  		<li>faire relire vôtre présentation à quelqu'un (directeur de thèse, autres doctorants, etc...).</li>
+  	</ul>
+  </p>
+  <p>
+  	Pour aller plus loin, voilà une <a href="http://www.its.caltech.edu/~matilde/Ma10Notes.pdf" target="_blank">référence en anglais</a> bien écrite et amusante qui donne quelques conseils pour faire une bonne présentation. Initialement elle  concerne les mathématiques et la physique théorique, mais s’adapte très bien à d’autres disciplines. Le lien : 
+  </p>
 </template>
