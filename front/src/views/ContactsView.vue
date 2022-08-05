66d18e2c03745112e68fddbbbbba5c1516b54f86
jacuminthomas666 <thomas.jacumin@uha.fr>
2022-08-05 10:13:18 +0200

    Put text in contacts page

--- front/src/views/ContactsView.vue
+++ front/src/views/ContactsView.vue
@@ -3,5 +3,74 @@
 </script>
 
 <template>
-  contacts
+  <h2>Coordinateur</h2>
+  <b-card-group deck style="margin-bottom: 20px;">
+	  <b-card title="Armand Ley" class="mb-2" style="max-width: 20rem; min-width: 20rem;">
+	    <b-card-text>
+	      Mathématiques
+	    </b-card-text>
+	    <b-card-text>
+	      armand.ley@uha.fr
+	    </b-card-text>
+	  </b-card>
+	</b-card-group>
+
+  <h2>Responsables d'équipe</h2>
+  <b-card-group deck>
+
+	  <b-card title="Armand Ley" class="mb-2" style="max-width: 20rem; min-width: 20rem;">
+	    <b-card-text>
+	      Mathématiques
+	    </b-card-text>
+	    <b-card-text>
+	      armand.ley@uha.fr
+	    </b-card-text>
+	  </b-card>
+
+	  <b-card title="Armand Ley" class="mb-2" style="max-width: 20rem; min-width: 20rem;">
+	    <b-card-text>
+	      Mathématiques
+	    </b-card-text>
+	    <b-card-text>
+	      armand.ley@uha.fr
+	    </b-card-text>
+	  </b-card>
+
+	  <b-card title="Armand Ley" class="mb-2" style="max-width: 20rem; min-width: 20rem;">
+	    <b-card-text>
+	      Mathématiques
+	    </b-card-text>
+	    <b-card-text>
+	      armand.ley@uha.fr
+	    </b-card-text>
+	  </b-card>
+
+	  <b-card title="Armand Ley" class="mb-2" style="max-width: 20rem; min-width: 20rem;">
+	    <b-card-text>
+	      Mathématiques
+	    </b-card-text>
+	    <b-card-text>
+	      armand.ley@uha.fr
+	    </b-card-text>
+	  </b-card>
+
+	  <b-card title="Armand Ley" class="mb-2" style="max-width: 20rem; min-width: 20rem;">
+	    <b-card-text>
+	      Mathématiques
+	    </b-card-text>
+	    <b-card-text>
+	      armand.ley@uha.fr
+	    </b-card-text>
+	  </b-card>
+
+	  <b-card title="Armand Ley" class="mb-2" style="max-width: 20rem; min-width: 20rem;">
+	    <b-card-text>
+	      Mathématiques
+	    </b-card-text>
+	    <b-card-text>
+	      armand.ley@uha.fr
+	    </b-card-text>
+	  </b-card>
+
+	</b-card-group>
 </template>
