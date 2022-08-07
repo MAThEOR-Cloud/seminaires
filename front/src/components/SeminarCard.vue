b129faab741054b7330201445872bba7735bebe2
jacuminthomas666 <thomas.jacumin@uha.fr>
2022-08-07 14:39:46 +0200

    modify seminar card design

--- front/src/components/SeminarCard.vue
+++ front/src/components/SeminarCard.vue
@@ -47,19 +47,23 @@ export default {
     <template class="card-header" #header>
       <h4 class="mb-0">{{ seminarModel?.title }}</h4>
     </template>
-    <b-card-text>
-      <img src="@/assets/calendar3.svg" style="margin-right: 5px;" /> {{ parseTimestamp(seminarModel?.date) }}
-      <img src="@/assets/watch.svg" style="margin-right: 5px; margin-left: 10px;" /> {{ seminarModel?.duration }}min
-    </b-card-text>
-    <b-card-text>
-      <img src="@/assets/person-fill.svg" style="margin-right: 5px;" /> {{ seminarModel?.author }}
-      <img src="@/assets/envelope.svg" style="margin-right: 5px; margin-left: 10px;" /> {{ seminarModel?.mail }}
-    </b-card-text>
-    <b-card-text>
-      <img src="@/assets/people-fill.svg" style="margin-right: 5px;" /> {{ seminarModel?.equip }}
-    </b-card-text>
-    <b-link href="#" class="card-link"><img style="width: 32px;" src="@/assets/file-earmark-pdf.svg" /></b-link>
-    <b-link href="#" class="card-link"><img style="width: 32px;" src="@/assets/easel2.svg" /></b-link>
+    <b-card-body class="card-body">
+      <b-card-text style="display: flex;">
+        <div style="width:50%;"><img src="@/assets/calendar3.svg" style="margin-right: 5px;" /> {{ parseTimestamp(seminarModel?.date) }}</div>
+        <div style="width:50%;"><img src="@/assets/watch.svg" style="margin-right: 5px; margin-left: 10px;" /> {{ seminarModel?.duration }}min</div>
+      </b-card-text>
+      <b-card-text style="display: flex;">
+        <div style="width:50%;"><img src="@/assets/person-fill.svg" style="margin-right: 5px;" /> {{ seminarModel?.author }}</div>
+        <div style="width:50%;"><img src="@/assets/envelope.svg" style="margin-right: 5px; margin-left: 10px;" /> {{ seminarModel?.mail }}</div>
+      </b-card-text>
+      <b-card-text style="display: flex;">
+        <img src="@/assets/people-fill.svg" style="margin-right: 5px;" /> {{ seminarModel?.equip }}
+      </b-card-text>
+      <b-card-text style="display: flex; justify-content: flex-end;">
+      <b-link href="#" class="card-link"><img style="width: 32px;" src="@/assets/file-earmark-pdf.svg" /></b-link>
+      <b-link href="#" class="card-link"><img style="width: 32px;" src="@/assets/easel2.svg" /></b-link>
+      </b-card-text>
+    </b-card-body>
   </b-card>
 </template>
 
@@ -69,5 +73,17 @@ export default {
   box-shadow: 0px 0px 2px #aaaaaa;
   margin-bottom: 15px;
 }
+
+.card-body {
+  padding: 0;
+}
+
+@media (min-width: 1024px) {
+  .card-body {
+    padding-right: 50px;
+    padding-left: 50px;
+  }
+}
+
 
 </style>
