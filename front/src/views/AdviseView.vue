43978cee9faa918af72faaf1a675cf46c4a90a5b
jacuminthomas666 <thomas.jacumin@uha.fr>
2022-08-28 18:56:13 +0200

    Add new text

--- front/src/views/AdviseView.vue
+++ front/src/views/AdviseView.vue
@@ -4,26 +4,61 @@
 
 <template>
   <h1 id="title">Conseils</h1>
-    <main>
-  	<p>
-    	Cette page a pour objectif de donner quelques conseils pour faire un exposé qui soit adapté à l’auditoire et qui évite les écueils les plus fréquents.
-    </p>
-    <p>
-    	<ul>
-    		<li>pas plus d'une slide par minutes ;</li>
-    		<li>ne pas charger trop les slides ;</li>
-    		<li>ne pas chercher à faire une revue exhaustive de la littérature ;</li>
-    		<li>ne pas chercher à présenter tout vos résultats. Un seul résultat parlant et bien détaillé suffit ;</li>
-    		<li>ne pas hésiter à détailler un exemple ;</li>
-    		<li>essayer de donner de l'intuition par tout les moyens (exemple, schémas, dessins, calcul simple, etc...) ;</li>
-    		<li>quand vous utilisez du vocabulaire techniques, des abréviations ou des notations qui ne sont pas universelles, prenez le temps de l'expliquer à l'auditoire ;</li>
-    		<li>quand vous utilisez une formule, prenez quelques secondes pour l'expliquer ;</li>
-    		<li>quand vous présentez une illustration, expliquer bien ce que vous montrer (paramètres, variables, etc...) ;</li>
-    		<li>faire relire vôtre présentation à quelqu'un (directeur de thèse, autres doctorants, etc...).</li>
-    	</ul>
-    </p>
-    <p>
-    	Pour aller plus loin, voilà une <a href="http://www.its.caltech.edu/~matilde/Ma10Notes.pdf" target="_blank">référence en anglais</a> bien écrite et amusante qui donne quelques conseils pour faire une bonne présentation. Initialement elle  concerne les mathématiques et la physique théorique, mais s’adapte très bien à d’autres disciplines.
-    </p>
+  <main>
+    <div id="content">
+      <img src="@/assets/campus-de-l-illberg.jpg" />
+    	<p>
+      	Cette page a pour objectif de donner quelques conseils pour faire un exposé adapté à l’auditoire et qui évite les écueils les plus fréquents :
+      </p>
+      <p>
+      	<ul>
+      		<li>pas plus d'une slide par minutes ;</li>
+      		<li>ne pas charger trop les slides ;</li>
+      		<li>ne pas chercher à faire une revue exhaustive de la littérature ;</li>
+      		<li>ne pas chercher à présenter tout vos résultats. Un seul résultat parlant et bien détaillé suffit ;</li>
+      		<li>ne pas hésiter à détailler un exemple ;</li>
+      		<li>essayer de donner de l'intuition par tout les moyens (exemple, schémas, dessins, calcul simple, etc...) ;</li>
+      		<li>quand vous utilisez du vocabulaire techniques, des abréviations ou des notations qui ne sont pas universelles, prenez le temps de l'expliquer à l'auditoire. Sinon ne les utilisez pas... ;</li>
+      		<li>quand vous utilisez une formule, prenez quelques secondes pour l'expliquer ;</li>
+      		<li>quand vous présentez une illustration, expliquer bien ce que vous montrer (paramètres, variables, etc...) ;</li>
+      		<li>faire relire vôtre présentation à quelqu'un (directeur de thèse, autres doctorants, etc...).</li>
+      	</ul>
+      </p>
+      <p>
+      	Pour aller plus loin, voilà une <a href="http://www.its.caltech.edu/~matilde/Ma10Notes.pdf" target="_blank">référence</a> (en anglais) bien écrite et amusante qui donne quelques conseils pour faire une bonne présentation. Initialement elle concerne les mathématiques et la physique théorique, mais contient des éléments utiles pour d’autres champs disciplinaires.
+      </p>
+    </div>
   </main>
 </template>
+
+<style scoped>
+#content {
+  text-align: justify;
+  padding: 20px;
+  background-color: white;
+  box-shadow: 0px 0px 2px #aaaaaa;
+  border-radius: 5px;
+}
+
+#content img {
+  margin-bottom: 10px;
+  border-radius: 5px;
+  box-shadow: 0px 0px 2px #aaaaaa;
+  float: left;
+  margin-right: 50px;
+  width: 100%;
+  height: auto;
+}
+
+@media (min-width: 1024px) {
+  #content img {
+    height: 500px;
+    width: auto;
+    margin-bottom: 0px;
+  }
+
+  #content {
+    min-height: 540px;
+  }
+}
+</style>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -6,31 +6,42 @@
 	<h1 id="title">Séminaire des doctorants IRIMAS</h1>
 	<main>
 		<div id="content">
-			<div>
-				<img src="@/assets/campus-de-l-illberg.jpg" />
-			</div>
-			<div style="padding: 20px; background-color: white; box-shadow: 0px 0px 2px #555555; border-radius: 5px;">
-				<p>
-					Le séminaire a lieu toute les semaines à Mulhouse ou à Colmar, généralement le Jeudi. Sauf dans les cas où le présentateur ne peut pas être présent à Mulhouse ou Colmar, les présentations se font en présentiel.
-				</p>
-				<p>
-					Plusieurs formats de séminaire sont possibles :
-					<ul>
-						<li>une intervention de 30 minutes, avec 20 minutes de présentation et 10 minutes de questions ;</li>
-						<li>deux interventions de 30 minutes, avec chacune 20 minutes de présentation et 10 minutes de questions ;</li>
-						<li>une intervention de 45 minutes, avec 45 minutes de présentations et 15 minutes de questions.</li>
-					</ul>
-				</p>
-				<p>
-					L'IRIMAS est divisé en trois départements, qui sont eux-mêmes divisés en équipes. Chaques <a href="https://www.irimas.uha.fr/index.php/equipes/" target="_blank">équipe</a> a un représentant. Le rôles des représentants est de conseiller et informer les gens dans son équipe et de faire remonter des informations au coordinateur. Le responsable de chaque équipe fera aussi chairman lorsque quelqu’un de son équipe présente.
-				</p>
-				<p>
-					Pour demander à faire une présentation, il suffit d'envoyer un mail au coordinateur en lui précisant la date où vous voudriez faire vôtre présentation.
-				</p>
-				<p>
-					Pour les créneaux libre, voir le <b-link to="/schedule">programme</b-link>.
-				</p>
-			</div>
+			<img src="@/assets/campus-de-l-illberg.jpg" />
+			<p>
+				Bienvenue sur le site du séminaire des doctorants de l’IRIMAS ! Sur cette page d’accueil, vous trouverez quelques informations sur le fonctionnement du séminaire.
+			</p>
+			<p>
+				 L'IRIMAS est divisé en trois départements, qui sont eux-mêmes divisés en <a href="https://www.irimas.uha.fr/index.php/equipes/" target="_blank">équipes</a>  (pour plus d’informations voir le site de l’IRIMAS). Chaque équipe a un représentant. Le rôle du représentant d’équipe est de conseiller et d’informer les gens dans son équipe et de faire remonter des informations au coordinateur du séminaire. Le responsable de chaque équipe anime la session lorsque c’est quelqu’un de son équipe qui présente. Le coordinateur du séminaire s’occupe du bon déroulement de l’ensemble, en lien avec les responsables d’équipes, la direction de l’IRIMAS et l’école doctorale.
+			</p>
+			<p>
+				Tout le monde peut assister au séminaire : doctorants, chercheurs, stagiaires de master (de l’IRIMAS ou de l’extérieur).
+			</p>
+			<p>
+				Le séminaire a lieu toutes les semaines à Mulhouse ou à Colmar, généralement le Jeudi. Hormis les cas où le présentateur ne peut pas être présent à Mulhouse ou Colmar, les présentations se font en présentiel.
+			</p>
+			<p>
+				Plusieurs formats de séminaire sont possibles :
+				<ul>
+					<li>une intervention de 30 minutes, avec 20 minutes de présentation et 10 minutes de questions ;</li>
+					<li>deux interventions de 30 minutes, avec chacune 20 minutes de présentation et 10 minutes de questions ;</li>
+					<li>une intervention de 45 minutes, avec 45 minutes de présentations et 15 minutes de questions.</li>
+				</ul>
+			</p>
+			<p>
+				Pour demander à faire une présentation, il suffit d'envoyer un mail au coordinateur (voir contact) en lui précisant la date où vous voudriez faire vôtre présentation. Pour les disponibilités, voir <b-link to="/schedule">ici</b-link>.
+			</p>
+			<p>
+				S’il en fallait, voici quelques raisons pour participer au séminaire :
+				<ul>
+					<li>travailler sa capacité à faire des présentations dans un cadre bienveillant ;</li>
+					<li>prendre au sérieux la tâche de diffusion du savoir scientifique (pour ceux qui présentent) ;</li>
+					<li>savoir sur quoi travaillent les gens du même laboratoire ;</li>
+					<li>augmenter sa culture scientifique général ;</li>
+					<li>amorcer d’éventuelles collaborations ;</li>
+					<li>rencontrer de nouvelles personnes et parler de science ;</li>
+					<li>ajouter une ligne sur un curriculum vitae.</li>
+				</ul>
+			</p>
 		</div>
 	</main>
 </template>
@@ -38,27 +49,26 @@
 <style scoped>
 #content {
 	text-align: justify;
-	display: flex;
-	align-items: center;
-	flex-direction: column;
-}
-
-#content img {
-	width: 100%;
-	margin-bottom: 10px;
+	padding: 20px;
+	background-color: white;
+	box-shadow: 0px 0px 2px #aaaaaa;
 	border-radius: 5px;
 }
 
-@media (min-width: 1024px) {
-  #content {
-    flex-direction: row;
-  }
+#content img {
+	margin-bottom: 10px;
+	border-radius: 5px;
+	box-shadow: 0px 0px 2px #aaaaaa;
+	float: left;
+	margin-right: 50px;
+	width: 100%;
+	height: auto;
+}
 
+@media (min-width: 1024px) {
   #content img {
+  	height: 500px;
   	width: auto;
-	  height: auto;
-	  max-width: 200%;
-	  max-height: 200%;
 	  margin-bottom: 0px;
   }
 }
