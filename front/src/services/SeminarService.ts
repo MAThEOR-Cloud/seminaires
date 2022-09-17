6be4a2e0b8f7cc112adea845ac1af4e0e3eb1be7
Armand Ley <armand67@laposte.net>
2022-09-17 15:31:25 +0200

    ajout slide Kenza

--- front/src/services/SeminarService.ts
+++ front/src/services/SeminarService.ts
@@ -5,11 +5,11 @@ export default class SeminarService {
 		return new Promise<SeminarModel[]>((resolve, reject) => {
 			resolve([
 	    	{ id: 17, title: "Automatic Machine Learning with Metaheuristics", author: "Ali Muhammad Junaid", equip: "OMEGA", date: 1657188600, duration: 30, mail: "", abstract: true, slides: false, },
-	    	{ id: 16, title: "Toward an Optimal Consensus Protocol for Private Blockchains", author: "Kenza Riahi", equip: "OMEGA", date: 1656585600, duration: 30, mail: "", abstract: true, slides: false, },
+	    	{ id: 16, title: "Toward an Optimal Consensus Protocol for Private Blockchains", author: "Kenza Riahi", equip: "OMEGA", date: 1656585600, duration: 30, mail: "", abstract: true, slides: true, },
 	    	{ id: 15, title: "Data Analysis Frameworks for Patients Monitoring and Assessment in Connected Healthcare Applications", author: "Ghina Saad", equip: "MSD", date: 1655980800, duration: 30, mail: "", abstract: true, slides: false, },
 	    	{ id: 14, title: "Depth-varying joint segmentation/deconvolution of vines’ wood in fluorescence microscopy", author: "Ouali Sonia", equip: "IMTI", date: 1655376000, duration: 30, mail: "", abstract: true, slides: false, },
 	    	{ id: 13, title: "Étude de comportement multi-passerelle pour Un Déploiement LoRaWAN orienté trafic", author: "Abakar Kerima Saleh", equip: "IMTI", date: 1654771200, duration: 30, mail: "", abstract: true, slides: false, },
-	    	{ id: 12, title: "Image Compression by Partial Differential Equations", author: "Thomas Jacumin", equip: "Analyse", date: 1654769400, duration: 30, mail: "thomas.jacumin@uha.fr", abstract: true, slides: false, },
+	    	{ id: 12, title: "Image Compression by Partial Differential Equations", author: "Thomas Jacumin", equip: "Analyse", date: 1654769400, duration: 30, mail: "thomas.jacumin@uha.fr", abstract: true, slides: true, },
 	    	{ id: 11, title: "Intelligent document information extraction based on convolutional neural network (CNN), natural language processing (NLP) and graph neural network (GNN)", author: "Mouad Hamri", equip: "MSD", date: 1654166400, duration: 30, mail: "", abstract: true, slides: false, },
 	    	{ id: 10, title: "A comparative study of newly developed metaheuristics for the discrete uncapacitated p-median problem", author: "Muhammad Sulaman", equip: "OMEGA", date: 1654164600, duration: 30, mail: "", abstract: true, slides: false, },
 	    	{ id: 9, title: "Piece-wise 3D reconstruction of Euler spirals from planar polygonal curves.", author: "Ali Fakih", equip: "OMEGA", date: 1652956800, duration: 30, mail: "", abstract: true, slides: false, },
